43d09b001c8ec4f68306e57575afaa0bfea58376
Samuel Simião <67723030+ssimiao@users.noreply.github.com>
2023-11-20 13:09:54 -0300

    Update GreetingResourceTest.java

--- leia/src/test/java/br/com/GreetingResourceTest.java
+++ leia/src/test/java/br/com/GreetingResourceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@QuarkusTest
 public class GreetingResourceTest {
 
     @Test
