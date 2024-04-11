253af60780098d8a7127c1f954ec162187d534fa
SimiaoSamuel <sssimiao10@gmail.com>
2024-04-11 16:23:06 -0300

    reflection

--- leia/src/main/java/br/com/shared/salesforce/Game.java
+++ leia/src/main/java/br/com/shared/salesforce/Game.java
@@ -2,8 +2,10 @@ package br.com.shared.salesforce;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@RegisterForReflection
 public class Game {
 
     @JsonProperty("contentUrl")
